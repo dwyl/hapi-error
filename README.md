c09c08bdfaecc3270acbeb07388e6edad5e11580
nelsonic <contact.nelsonic+github@gmail.com>
2016-04-23 17:41:11 +0100

    adds initial files with server_example.js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hapi-error
+
 Intercept errors in your Hapi web app/api and send a *useful* message to the client.
+
+## Why?
+
+By default, Hapi does not give people *friendly* error messages. 
---
+++ test/server_example.js
@@ -0,0 +1,53 @@
+var Hapi = require('hapi');
+var Path = require('path');
+
+var server = new Hapi.Server();
+server.connection({ port: process.env.PORT });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply(new Error('500'));
+    }
+});
+
+server.ext('onPreResponse', function (request, reply) {
+
+    if (request.response.isBoom) {
+        var err = request.response;
+        var statusCode = err.output.payload.statusCode
+
+
+        return reply.view('error_template', {
+            title: 'Server Error',
+            statusCode: statusCode,
+            errorName:  err.output.payload.error,
+            errorMessage: 'Sorry, Something went wrong.'
+        })
+        .code(statusCode);
+    }
+    reply.continue();
+});
+
+
+server.register(require('vision'), function (err) {
+
+    if (err) {
+        throw err;
+    }
+    server.views({
+        engines: {
+          html: require('handlebars')
+        },
+        path: Path.resolve(__dirname, './../lib')
+    });
+
+    server.start(function (err) {
+
+        if (err) {
+            throw err;
+        }
+        console.log('Visit:', server.info.uri);
+    });
+});
