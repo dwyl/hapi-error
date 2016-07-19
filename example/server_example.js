efff983ee914c0e416430f370ab3ea42de3412b9
nelsonic <contact.nelsonic+github@gmail.com>
2016-07-19 13:46:28 +0100

    [WiP] adds example route + test for https://github.com/dwyl/hapi-error/issues/9

--- example/server_example.js
+++ example/server_example.js
@@ -2,6 +2,7 @@ var Hapi = require('hapi');
 var Path = require('path');
 var Boom = require('boom');
 var Hoek = require('hoek');
+var Joi = require('joi');
 
 var server = new Hapi.Server();
 server.connection({ port: process.env.PORT });
@@ -40,7 +41,25 @@ server.route([
     config: {
       handler: function (request, reply) {
         var err = true; // force error using hoek
-        Hoek.assert(!err, 'Boom Goes the Dynamite!');
+        return Hoek.assert(!err, 'Boom Goes the Dynamite!');
+        // no reply because Hoek fires an error!
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/register/{param*}',
+    config: {
+      validate: { params: { param: Joi.string().min(4).max(160).alphanum() } },
+      handler: function (request, reply) {
+        console.log(request.params.param);
+        // apply additional check to the param and throw error if invalid ...
+        if(request.params.param.indexOf('script') > -1) {
+          var err = true; // force error using hoek
+          return Hoek.assert(!err, 'Boom Goes the Dynamite!');
+        } else {
+          return reply('Hello ' + request.params.param + '!')
+        }
         // no reply because Hoek fires an error!
       }
     }
