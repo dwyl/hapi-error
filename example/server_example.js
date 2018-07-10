5db6e90fb859c6af0300adb7e0121c1c7607e56b
Tejus Pratap <tejzpr@gmail.com>
2018-07-10 00:21:29 -0700

    Update server_example.js

--- example/server_example.js
+++ example/server_example.js
@@ -1,24 +1,22 @@
 var server = require('./server.js');
 var Hoek   = require('hoek');
 
-server.register(
-  [
-    require('../lib/index.js'),
-    require('vision')
-  ],
-  function (err) {
+(async () => {
+
+    await server.register(require('vision'));
+    await server.register(require('../lib/index.js'));
     server.views({
-      engines: {
-        html: require('handlebars')
-      },
-      path: require('path').resolve(__dirname, './')
+        engines: {
+            html: require('handlebars')
+        },
+        path: require('path').resolve(__dirname, './')
     });
     Hoek.assert(!err, 'no errors registering plugins');
+  
+    server.start(function (err) {
+    Hoek.assert(!err, 'no errors starting server');
+    server.log('info', 'Visit: ' + server.info.uri);
 });
-
-server.start(function (err) {
-  Hoek.assert(!err, 'no errors starting server');
-  server.log('info', 'Visit: ' + server.info.uri);
-});
+})();
 
 module.exports = server;
